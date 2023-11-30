52eb27921ddc455ebd971b1db16ba264a9e5933e
Gato <115658935+CollectiveUnicorn@users.noreply.github.com>
2023-11-29 16:17:53 -0800

    18 - Fixes model selection and switches to typescript (#20)
    * Updates backend to use model selected by user and bumps openai to 4.20.1
    
    * Fixes the model selection and switches app to typescript
    
    * Removes stream option on the frontend and commented console logs

--- src/routes/api/chat-completion/+server.ts
+++ src/routes/api/chat-completion/+server.ts
@@ -14,12 +14,10 @@ export async function POST(event: RequestEvent) {
         const data = await event.request.json()
         const messages = data["messages"];
 
-        // console.log(data)
-
         // Make a request to OpenAI's Chat Completion API
         const chatCompletion = await openai.chat.completions.create({
             messages: messages,
-            model: configs.DEFAULT_MODEL,
+            model: data["model"],
             temperature: data["temperature"],
             max_tokens: data["max_tokens"],
             stream: true
@@ -30,7 +28,6 @@ export async function POST(event: RequestEvent) {
                 for await (const part of chatCompletion) {
                     const content = part.choices[0]['delta'].content;
                     controller.enqueue(content || '');
-                    // console.log(content || '')
                 }
                 controller.close();
             }
