81a2ca76e4aa62ef4bc088c82e67ca9eddbece5f
Gato <115658935+CollectiveUnicorn@users.noreply.github.com>
2023-12-01 13:59:14 -0800

    Moves openai variables so that they load at runtime (#25)
    * Handles unset variable for PUBLIC_URL_PREFIX
    * Moves openai variables so that they load at runtime for better compatibility with docker compose

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,5 +1,6 @@
 import {env} from "$env/dynamic/public";
 
 export function urlConcat(url: string) : string {
-    return env.PUBLIC_URL_PREFIX + url;
+
+    return (env.PUBLIC_URL_PREFIX === undefined ? "" : env.PUBLIC_URL_PREFIX) + url;
 }
