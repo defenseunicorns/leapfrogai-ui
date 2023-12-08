ebf0eb0ad3e5feb030b2db8ccdb48dbef6451488
Gato <115658935+CollectiveUnicorn@users.noreply.github.com>
2023-12-07 16:28:06 -0800

    Adds markdown and code formatting to chat (#32)

---
+++ src/lib/components/codeblock.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {HighlightAuto} from "svelte-highlight";
+    import github from "svelte-highlight/styles/github";
+    import {FileCopyOutline} from "flowbite-svelte-icons";
+
+    export let lang: string, text: string;
+
+    function copyToClipboard() {
+        navigator.clipboard.writeText(text).catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+    }
+</script>
+
+<svelte:head>
+    {@html github}
+</svelte:head>
+
+<div class="code-block">
+    <div class="flex justify-between">
+        <span class="language-label">{lang}</span>
+        <button class="copy-button align-middle" on:click={copyToClipboard}>
+            <FileCopyOutline ariaLabel="Copy Code" class="align-middle justify-center" size="sm"/>
+        </button>
+
+    </div>
+    <HighlightAuto code="{text}"/>
+</div>
