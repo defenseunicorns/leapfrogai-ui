5b4f9aaa41e21a978d4dfd0446ec10fd5d52839d
Gregory <gphorvath@defenseunicorns.com>
2024-01-03 16:29:41 -0500

    Change default theme (#38)
    * switched to black theme from DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./src/**/*.{svelte,html,js}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
   darkMode: 'class',
 
-  theme: {
-    extend: {
-      colors: {
-      }
-    }
-  },
   plugins: [require("daisyui"), require('flowbite/plugin')],
-}
 
+  daisyui: {
+    themes: ["black", "wireframe"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    lightTheme: "wireframe", // default light theme
+    darkTheme: "black", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+}
