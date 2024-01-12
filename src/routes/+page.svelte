fb6a736442b833d6df2e0f72c4f213695419f5e7
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-01-11 23:17:02 -0500

    Add landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<div class="flex flex-col h-screen">
+    <div class="flex flex-grow">
+        <div
+            class="w-full pb-4 pt-4 flex flex-col ml-72 mr-72 mt-20 mb-20 justify-center items-center"
+        >
+            <div class="text-5xl font-bold text-center">
+                <p>Welcome to LeapfrogAI</p>
+            </div>
+            <div class="justify-center items-center">
+                <img src="big-leapfrog.svg" />
+            </div>
+            <div class="text-2xl font-bold text-center">
+                <p>Created and Maintained by:</p>
+            </div>
+            <img src="defense-unicorns.svg" class="h-11" />
+        </div>
+    </div>
+</div>
