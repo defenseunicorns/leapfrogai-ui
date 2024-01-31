229df28990a7718845dcd078d0b61cd886650255
Gato <115658935+CollectiveUnicorn@users.noreply.github.com>
2024-01-31 17:35:42 -0500

    Adds option to throttle concurrent llm requests (#95)
    * Adds the variable `CONCURRENT_REQUESTS` that allows for limiting the concurrent generation to 1 user at a time via a lockfile on the server.

--- src/routes/api/chat-completion/+server.ts
+++ src/routes/api/chat-completion/+server.ts
@@ -1,21 +1,25 @@
 import OpenAI from 'openai';
 import type { RequestEvent } from '@sveltejs/kit';
 import {env} from "$env/dynamic/private";
+import {FileLock} from "$lib/interfaces/file_lock";
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST(event: RequestEvent) {
+    const mutex: FileLock = new FileLock(env.CONCURRENT_REQUESTS);
+
     const openai = new OpenAI({
         apiKey: env.OPENAI_API_KEY,
         baseURL: env.OPENAI_API_HOST
     });
 
     try {
-        const data = await event.request.json()
+        const data: ChatRequest = await event.request.json()
+        await mutex.lock();
         const messages = data["messages"];
 
         // Make a request to OpenAI's Chat Completion API
         const chatCompletion = await openai.chat.completions.create({
-            messages: messages,
+            messages: messages as OpenAI.Chat.Completions.ChatCompletionMessageParam[],
             model: data["model"],
             temperature: data["temperature"],
             max_tokens: data["max_tokens"],
@@ -24,9 +28,21 @@ export async function POST(event: RequestEvent) {
 
         const body = new ReadableStream({
             async pull(controller) {
+                let count: number = 0, lastCount = 0;
+                const interval = setInterval(() => {
+                    if (count > lastCount) {
+                        lastCount = count;
+                    } else {
+                        mutex.unlock()
+                        clearInterval(interval);
+                    }
+
+                }, 1000);
+
                 for await (const part of chatCompletion) {
                     const content = part.choices[0]['delta'].content;
                     controller.enqueue(content || '');
+                    count++;
                 }
                 controller.close();
             }
@@ -39,9 +55,9 @@ export async function POST(event: RequestEvent) {
             },
         });
 
-        // console.log(llmResponse.data)
     } catch (error) {
         console.error(error);
+        mutex.unlock()
 
         return new Response('Internal Server Error', {
             status: 500,
