2be4e27a12259ef578bff810067a8ee0e8adea6f
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-02-07 16:59:26 -0500

    added branding env to switch between AI4NS and LFAI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import "../app.css";
 	import ThemeSwitcher from "$lib/components/themeSwitcher.svelte";
-    import ApiIndicator from "$lib/components/apiIndicator.svelte";
-	import settings from '$lib/settings';
+	import ApiIndicator from "$lib/components/apiIndicator.svelte";
+	import settings from "$lib/settings";
+	import { env } from "$env/dynamic/public";
+
+	const branding: Boolean = env.PUBLIC_BRANDING === "true" ? true : false;
 </script>
 
 <!-- Title Bar -->
@@ -10,21 +13,27 @@
 	class="fixed top-0 w-full flex items-center justify-between p-4 border-b border-inherit bg-base-100"
 >
 	<div class="flex items-center">
-		<a href="/">
-			<img src="leapfrogai.png" alt="LeapfrogAI" class="h-11" />
-			<!-- <img src="DU_unicorn_rgb.png" alt="AI for National Security" class="h-11" /> -->
-		</a>
-		<!-- <div style="margin-left: 8px;">
+		{#if branding}
+			<a href="/">				
+				<img src="DU_unicorn_rgb.png" alt="AI for National Security" class="h-11" />
+			</a>
+					<div style="margin-left: 8px;">
 			<h1 class="text-xl font-bold">AI for National Security</h1>
-		</div> -->
+		</div>
+		{/if}
+		{#if !branding}
+			<a href="/">
+				<img src="leapfrogai.png" alt="LeapfrogAI" class="h-11" />
+			</a>
+		{/if}
 	</div>
 	<div class="flex items-center">
 		<a href="/chat">Chat</a>
-		<ApiIndicator type={$settings.chatModel}/>
+		<ApiIndicator type={$settings.chatModel} />
 	</div>
 	<div class="flex items-center">
 		<a href="/upload">Transcribe</a>
-		<ApiIndicator type={$settings.transcriptionModel}/>
+		<ApiIndicator type={$settings.transcriptionModel} />
 	</div>
 	<div>
 		<a href="/help">Help</a>
