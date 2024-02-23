b0b4e9083ee0a811cc811dc16b6f74dca289bf87
michael slaughter <mikeslaughter@defenseunicorns.com>
2024-02-23 12:03:06 -0700

    fixed being able to type messages

--- src/lib/components/personaPicker.svelte
+++ src/lib/components/personaPicker.svelte
@@ -1,25 +1,61 @@
 <script lang="ts">
-    import {
-        EditOutline,
-        PlusOutline,
-        TrashBinOutline,
-    } from "flowbite-svelte-icons";
     import { env } from "$env/dynamic/public";
-    import { writable } from "svelte/store";
     import { onMount } from "svelte";
     import { v4 as uuidv4 } from "uuid";
-    import ModelPicker from "./modelPicker.svelte";
 
     export let pickedPersona: Agent;
 
     let localStorage: Storage;
-    let personas = writable([]);
-
-    let personaSearchQuery = "";
+    let personas = [
+        {
+            uuid: uuidv4(),
+            name: "HR Representative",
+            description: "Description for Persona 1",
+            model: env.PUBLIC_DEFAULT_MODEL,
+            url: env.PUBLIC_OPENAI_API_HOST,
+            type: "Chat",
+            systemPrompt: env.PUBLIC_DEFAULT_SYSTEM_PROMPT,
+            temperature: Number(env.PUBLIC_DEFAULT_TEMPERATURE),
+            rag_enabled: false,
+        },
+        {
+            uuid: uuidv4(),
+            name: "LeapfrogAI Dev",
+            description: "Description for Persona 2",
+            model: env.PUBLIC_DEFAULT_MODEL,
+            url: env.PUBLIC_OPENAI_API_HOST,
+            type: "Chat",
+            systemPrompt: env.PUBLIC_DEFAULT_SYSTEM_PROMPT,
+            temperature: Number(env.PUBLIC_DEFAULT_TEMPERATURE),
+            rag_enabled: false,
+        },
+        {
+            uuid: uuidv4(),
+            name: "Growth Engineer",
+            description: "Description for Persona 3",
+            model: env.PUBLIC_DEFAULT_MODEL,
+            url: env.PUBLIC_OPENAI_API_HOST,
+            type: "Chat",
+            systemPrompt: env.PUBLIC_DEFAULT_SYSTEM_PROMPT,
+            temperature: Number(env.PUBLIC_DEFAULT_TEMPERATURE),
+            rag_enabled: false,
+        },
+        {
+            uuid: uuidv4(),
+            name: "Website Engineer",
+            description: "Description for Persona 4",
+            model: env.PUBLIC_DEFAULT_MODEL,
+            url: env.PUBLIC_OPENAI_API_HOST,
+            type: "Chat",
+            systemPrompt: env.PUBLIC_DEFAULT_SYSTEM_PROMPT,
+            temperature: Number(env.PUBLIC_DEFAULT_TEMPERATURE),
+            rag_enabled: false,
+        },
+    ];
 
     let defaultSettings: Agent = {
         uuid: uuidv4(),
-        name: "New Persona",
+        name: "HR Representative",
         description: "",
         model: env.PUBLIC_DEFAULT_MODEL,
         url: env.PUBLIC_OPENAI_API_HOST,
@@ -38,15 +74,15 @@
         if (localStorage) {
             const storedPersonas = JSON.parse(localStorage.getItem("personas"));
             if (storedPersonas?.length > 0) {
-                personas.set(storedPersonas);
+                personas = storedPersonas;
             }
         }
 
-        if ($personas.length === 0) {
-            personas.set([defaultSettings]);
+        if (personas.length === 0) {
+            personas = [defaultSettings];
         }
 
-        pickedPersona = $personas[0];
+        pickedPersona = personas[0];
     }
 
     function persistPersonas(value: any[]) {
@@ -55,220 +91,32 @@
         }
     }
 
-    personas.subscribe(persistPersonas);
-
-    function newPersona() {
-        defaultSettings.uuid = uuidv4();
-        personas.update((n: any[]) => [
-            ...n,
-            {
-                ...defaultSettings,
-            },
-        ]);
-        editPersona(defaultSettings.uuid);
-    }
+    getLocalPersonas();
 
     function applyPersona(persona: any) {
         pickedPersona = persona;
-    }
-
-    function removePersona(id: any) {
-        personas.update((n: any[]) => n.filter((p) => p.uuid !== id));
-    }
-
-    let tmpNewPersona = defaultSettings;
-    function editPersona(id: any) {
-        const persona = $personas.find((p) => p.uuid === id);
-        tmpNewPersona = { ...persona };
-        document.getElementById("persona_modal")["showModal"]();
-    }
-    function updatePersona(id: any) {
-        personas.update((n) => {
-            return n.map((p) => {
-                if (p.uuid === id) {
-                    // Return a new object with the updated properties
-                    return {
-                        ...p,
-                        name: tmpNewPersona.name,
-                        description: tmpNewPersona.description,
-                        model: tmpNewPersona.model,
-                        url: tmpNewPersona.url,
-                        type: tmpNewPersona.type,
-                        systemPrompt: tmpNewPersona.systemPrompt,
-                        temperature: tmpNewPersona.temperature,
-                        rag_enabled: tmpNewPersona.rag_enabled,
-                    };
-                }
-                return p; // Return the original object for other personas
-            });
-        });
-        document.getElementById("persona_modal")["close"]();
+        console.log(pickedPersona)
     }
 </script>
 
 <div class="w-72 p-4 h-full fixed top-20 right-0 overflow-y-auto">
-    <!-- <button
-        class="btn w-full mb-2 justify-between"
-        on:click={() => {
-            newPersona();
-        }}
-        >New Persona <PlusOutline />
-    </button> -->
-    <!-- <input
-        class="input input-bordered w-full mb-2"
-        type="text"
-        placeholder="Search"
-        bind:value={personaSearchQuery}
-    /> -->
-    {#each $personas as persona}
-        {#if personaSearchQuery == "" || persona.name
-                .toLowerCase()
-                .includes(personaSearchQuery.toLowerCase())}
+    {#each personas as persona}
+    
             <div class="menu bg-base-100 w-full rounded-box">
                 <div class="flex">
-                    <li class="">
+                    <li class="w-4/6 flex-nowrap">
                         <button
-                            class= 'button'
+                            class="whitespace-nowrap {persona.uuid === pickedPersona.uuid ? 'outline outline-2 outline-offset-2 outline-secondary' : ''}"
                             on:click={() => {
                                 applyPersona(persona);
-                            }}>HR Representative</button
-                        >
+                                console.log(persona.name)
+                            }}>{persona.name}</button>
                     </li>
-                    <li class="">
-                        <button
-                        class= 'button'
-                            on:click={() => {
-                                applyPersona(persona);
-                            }}>LeapfrogAI Dev</button
-                        >
-                    </li>
-                    <li class="">
-                        <button
-                            class='button'
-                            on:click={() => {
-                                applyPersona(persona);
-                            }}>Growth Engineer</button
-                        >
-                    </li>
-                    <li class="">
-                        <button
-                            class='button'
-                            on:click={() => {
-                                applyPersona(persona);
-                            }}>Website Engineer</button
-                        >
-                    </li>
-
-                    <!-- <button
-                        class="btn-ghost w-1/6 px-2"
-                        on:click={() => {
-                            editPersona(persona.uuid);
-                        }}><EditOutline /></button
-                    > -->
-                    <!-- <button
-                        class="btn-ghost w-1/6 px-2"
-                        on:click={() => {
-                            removePersona(persona.uuid);
-                        }}><TrashBinOutline /></button
-                    > -->
                 </div>
             </div>
-        {/if}
     {/each}
 </div>
 
-<!-- {#if $personas.length > 0}
-    <dialog id="persona_modal" class="modal">
-        <div class="modal-box">
-            <h3 class="font-bold text-lg">
-                {tmpNewPersona.name}
-            </h3>
-            <form
-                on:submit|preventDefault={() =>
-                    updatePersona(tmpNewPersona.uuid)}
-            >
-                <div class="py-2">
-                    <label for="persona-name">Name:</label>
-                    <input
-                        id="persona-name"
-                        type="text"
-                        bind:value={tmpNewPersona.name}
-                        class="input input-bordered w-full max-w-xs mb-2"
-                    />
-                </div>
-                <div class="py-2">
-                    <label for="persona-description">Description:</label>
-                    <input
-                        id="persona-description"
-                        type="text"
-                        bind:value={tmpNewPersona.description}
-                        class="input input-bordered w-full max-w-xs mb-2"
-                    />
-                </div>
-                <div class="py-2">
-                    <ModelPicker bind:selectedModel={tmpNewPersona.model} />
-                </div>
-                <div class="py-2">
-                    <label for="persona-url">URL:</label>
-                    <input
-                        id="persona-url"
-                        type="text"
-                        bind:value={tmpNewPersona.url}
-                        class="input input-bordered w-full max-w-xs mb-2"
-                    />
-                </div>
-                <div class="py-2">
-                    <label for="persona-type">Type:</label>
-                    <input
-                        id="persona-type"
-                        type="text"
-                        bind:value={tmpNewPersona.type}
-                        class="input input-bordered w-full max-w-xs mb-2"
-                    />
-                </div>
-                <div class="py-2">
-                    <label for="persona-system-prompt">System Prompt:</label>
-                    <input
-                        id="persona-system-prompt"
-                        type="text"
-                        bind:value={tmpNewPersona.systemPrompt}
-                        class="input input-bordered w-full max-w-xs mb-2"
-                    />
-                </div>
-                <div class="py-2">
-                    <label for="persona-temperature">Temperature:</label>
-                    <input
-                        id="persona-temperature"
-                        type="range"
-                        min="0"
-                        max="1"
-                        step="0.01"
-                        bind:value={tmpNewPersona.temperature}
-                    />
-                </div>
-                <div class="py-2">
-                    <label for="persona-rag-enabled">RAG Enabled:</label>
-                    <input
-                        class="checkbox checkbox-xs"
-                        id="persona-rag-enabled"
-                        type="checkbox"
-                        bind:checked={tmpNewPersona.rag_enabled}
-                    />
-                </div>
-                <div class="modal-action">
-                    <button type="submit" class="btn">Save</button>
-                    <button
-                        type="button"
-                        class="btn"
-                        on:click={() => {
-                            document.getElementById("persona_modal")["close"]();
-                        }}>Cancel</button
-                    >
-                </div>
-            </form>
-        </div>
-    </dialog>
-{/if} -->
 
 <style>
     .flex{
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,142 @@
 <script lang="ts">
-    import "../app.css";
-    import { env } from "$env/dynamic/public";
 
-    const branding: Boolean = env.PUBLIC_AI4NS_BRANDING === "true" ? true : false;
+    import { onMount } from "svelte";
+    import { writable } from "svelte/store";
+    import { urlConcat } from "$lib/helper";
+    import PersonaPicker from "$lib/components/personaPicker.svelte";
+    import settings from '$lib/settings';
+    import type {ModelSettings} from '$lib/settings'
+    import { PUBLIC_TRANSCRIPTION_MODEL } from "$env/static/public";
+    import ChatPicker from "$lib/components/chatPicker.svelte";
+    import ChatPanel from "$lib/components/chatPanel.svelte";
+
+    let conversations = writable([]);
+    let fileInput;
+    let fileInputRag;
+    let ragEndpointActive = false;
+    let ragEnabled = false;
+
+    let currentSettings: Agent;
+
+    function updateModelIndicators(value: Agent) {
+        if (currentSettings) {
+            const modelSettings: ModelSettings = {
+                chatModel: value.model,
+                transcriptionModel: PUBLIC_TRANSCRIPTION_MODEL
+            }
+
+            settings.set(modelSettings)
+        }
+    }
+
+    $: updateModelIndicators(currentSettings)
+
+    onMount(async () => {
+        await updateRagEndpointState();
+    });
+
+    async function updateRagEndpointState() {
+        fetch(urlConcat("/api/rag/health"))
+            .then((response) => response.json())
+            .then((body) => {
+                ragEndpointActive = body.enabled;
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+
+    let currentConversationId = writable(null);
+
+    async function queryRag(query) {
+        try {
+            return await fetch(urlConcat("/api/rag/query"), {
+                method: "POST",
+                body: JSON.stringify({
+                    input: query,
+                    collection_name: "default",
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((response) => {
+                return response.text();
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function importFiles(event) {
+        const files = event.target.files;
+
+        for (let i = 0; i < files.length; i++) {
+            const file = event.target.files[i];
+            if (file) {
+                try {
+                    const formData = new FormData();
+                    formData.append("file", file);
+
+                    await fetch(urlConcat("/api/rag/upload"), {
+                        method: "POST",
+                        body: formData,
+                    });
+
+                    // Clear the file input for potential future use
+                    event.target.value = "";
+                } catch (error) {
+                    console.error("Error reading or parsing JSON:", error);
+                }
+            }
+        }
+    }
+
+    async function importData(event) {
+        const file = event.target.files[0];
+        if (file) {
+            try {
+                const fileText = await file.text();
+
+                conversations.set(JSON.parse(fileText));
+                // Clear the file input for potential future use
+                event.target.value = "";
+            } catch (error) {
+                console.error("Error reading or parsing JSON:", error);
+            }
+        }
+    }
 </script>
 
-{#if branding}
-    <title>AI for National Security</title>
-{/if}
-{#if !branding}
-    <title>LeapfrogAI</title>
-{/if}
+<svelte:head>
+    <title>Chat</title>
+</svelte:head>
 
 <div class="flex flex-col h-screen">
     <div class="flex flex-grow">
-        <div
-            class="w-full pb-4 pt-4 flex flex-col ml-72 mr-72 mt-20 mb-20 justify-center items-center"
-        >
-            {#if branding}
-                <div class="text-5xl font-bold text-center">
-                    <p>Welcome to AI for National Security</p>
-                </div>
-                <div class="justify-center items-center py-4">
-                    F
-                    <img src="server.svg" alt="" />
-                </div>
-            {/if}
-            {#if !branding}
-                <div class="text-5xl font-bold text-center">
-                    <p>Welcome to LeapfrogAI</p>
-                </div>
-                <div class="justify-center items-center py-4">
-                    F
-                    <img src="big-leapfrog.svg" alt="" />
-                </div>
-            {/if}
-            <div class="text-2xl font-bold text-center">
-                <p>Created and Maintained by:</p>
-            </div>
-            <img
-                src="defense-unicorns.svg"
-                class="h-11"
-                alt="Defense Unicorns"
-            />
-        </div>
+        <!-- Side Panel 1 -->
+        <ChatPicker bind:curConversationId={currentConversationId} bind:conversations={conversations}/>
+                {#if ragEndpointActive}
+                    <input
+                        type="file"
+                        accept=".txt,.pdf"
+                        on:change={importFiles}
+                        bind:this={fileInputRag}
+                        multiple={true}
+                        class="hidden"
+                    />
+                {/if}
+                <input
+                    type="file"
+                    accept=".json"
+                    on:change={importData}
+                    bind:this={fileInput}
+                    class="hidden"
+                />
+
+        <!-- Center Panel -->
+        <ChatPanel agentSettings={currentSettings} bind:curConversationId={currentConversationId} bind:conversations={conversations}/>
+
+        <!-- Side Panel 2 -->
+        <PersonaPicker bind:pickedPersona={currentSettings}/>
     </div>
 </div>
